3c30099cf8b56f420925c8aec82fdc3e0085a50f
root <yinrui123@163.com>
2020-08-20 09:35:01 +0800

    Commit Message

---
+++ HelloWord/src/com/oracle/test/gitmain.java
@@ -0,0 +1,10 @@
+package com.oracle.test;
+
+public class gitmain {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+}
